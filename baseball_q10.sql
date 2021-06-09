aaba7646408c9a9943b57bbd997f4b5d139a97bb
Dlstark <83980562+Dlstark@users.noreply.github.com>
2021-06-09 11:03:52 -0500

    Update baseball_q10.sql

--- baseball_q10.sql
+++ baseball_q10.sql
@@ -1,5 +1,5 @@
-SELECT DISTINCT s.schoolid, schoolstate, c.playerid, s.schoolname, 
-				p.namegiven, sal.salary, sal.yearid
+SELECT DISTINCT s.schoolname, p.namegiven, 
+		sal.salary:: numeric :: text::money, sal.yearid
 FROM schools as s LEFT JOIN collegeplaying as c
 using(schoolid)
 LEFT JOIN people as p
@@ -8,7 +8,7 @@ LEFT JOIN salaries as sal
 ON (c.playerid) = (sal.playerid)
 WHERE schoolstate = 'TN'
 AND sal.salary IS NOT NULL
-ORDER BY sal.salary DESC;
+ORDER BY sal.salary::numeric::text::money DESC;
 
 -- A: Vandy has the highest 1 year salary at 30,000,000 for David Taylor in 2016, 
 -- But UT is pretty comsistantly up there with Todd Lynn.  No direct winner, 
